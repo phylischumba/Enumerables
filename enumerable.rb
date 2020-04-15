6ea3efab016d8ed5979d716f0d39dd91d37ff696
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-04-15 13:22:02 +0300

    update methods to take arguments

--- enumerable.rb
+++ enumerable.rb
@@ -30,25 +30,25 @@ module Enumerable
   end
 
   def my_all?(*args)
-
     condition = true
     if !args[0].nil?
-      my_each {|i| condition = false}
+      my_each { |_i| condition = false }
     elsif !block_given?
-      my_each {|i| condition = false}
+      my_each { |_i| condition = false }
     else
       my_each { |i| condition = false unless yield(i) }
     end
     condition
   end
 
-  def my_any?
-    return to_enum(:my_any?) unless block_given?
-
+  def my_any?(*args)
     state = false
-    my_each do |i|
-      state = true if yield(i)
-      break
+    if !args[0].nil?
+      my_each { |i| state = true if args[0] === i }
+    elsif !block_given?
+      my_each { |i| state = true if i }
+    else
+      my_each { |i| state = true if yield(i) }
     end
     state
   end
@@ -65,8 +65,9 @@ module Enumerable
     return to_enum(:my_count) unless block_given?
 
     total = 0
-    my_each do |x|
-      total += 1 if yield(self[x])
+    my_each do |i|
+      total += 1 if block_given? && yield(i)
+      total = size unless block_given?
     end
     total
   end
@@ -83,22 +84,35 @@ module Enumerable
     mapped
   end
 
-  def my_inject
-    return to_enum(:my_inject) unless block_given?
-
-    result = self[0]
-    shift
-    my_each do |elem|
-      result = yield(result, elem)
+  def my_inject(*args)
+    arr = to_a
+    if !args.empty? && args[0].class != Symbol
+      acc = args[0]
+      arr.my_each { |item| acc = yield(acc, item) }
+    elsif args.empty?
+      acc = to_a[0]
+      arr[1..-1].my_each { |item| acc = yield(acc, item) }
+    elsif args[0].class == Symbol
+      acc = to_a[0]
+      # operation = args[0]
+      arr[1..-1].my_each { |item| acc = acc.yield(total, item) }
     end
-    result
-  end
-
-  def multiply_els
-    return to_enum(:multiply_els) unless block_given?
-
-    my_inject { |total, k| return total * k }
+    acc
   end
 end
+def multiply_els
+  return to_enum(:multiply_els) unless block_given?
 
-p %w[ant bear ca].my_none? {  }
+  my_inject(:*)
+end
+
+# p [].my_each { |i| puts i }
+
+# p ['burner'].my_none? { |word| word.length >= 3 }
+# p [].my_count(&:even?)
+# p (1..7).my_inject { |product, n| product * n }
+# longest = %w[cat sheep bear].my_inject do |memo, word|
+#   memo.length > word.length ? memo : word
+# end
+# p longest
+# p (5..10).inject { |sum, n| sum + n }
